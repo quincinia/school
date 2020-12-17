0f132f4bdb5ee7ba9053b105c99ee073cddf6aad
quincinia <eanj13@gmail.com>
2020-12-17 14:53:03 -0800

    fixing stuff

--- cs302-hw2-code-package/config.h
+++
@@ -1,7 +0,0 @@
-/**
- * Stack class configuration file.
- * Activate test #N by defining the corresponding LAB6_TESTN to have the value 1.
- */
-
-#define LAB6_TEST1	0	// 0 => use array implementation, 1 => use linked impl.
-
